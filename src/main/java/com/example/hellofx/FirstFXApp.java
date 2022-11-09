698be295c57c03211268187eb5648d6d0f071668
Fernando Lopez <flopez00032@gmail.com>
2022-11-08 21:38:24 -0600

    tutorial almost done

--- src/main/java/com/example/hellofx/FirstFXApp.java
+++ src/main/java/com/example/hellofx/FirstFXApp.java
@@ -7,8 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -42,40 +46,149 @@ public class FirstFXApp extends Application {
             this.y =y;
         }
     }
-
-
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Snake");
+        try{
+            newFood();
 
-        try
-        {
             VBox root = new VBox();
             Canvas c = new Canvas(with*cornerSize,height*cornerSize);
             GraphicsContext gc = c.getGraphicsContext2D();
             root.getChildren().add(c);
+            new AnimationTimer(){
+                long lastTick = 0;
 
-            new AnimationTimer();
-            long lastTick = 0;
-
-            public void handle(long now){
-                if(lastTick == 0){
-                    lastTick == now;
-                    return;
+                public void handle(long now){
+                    if(lastTick == 0){
+                        lastTick = now;
+                        tick(gc);
+                        return;
+                    }
+                    if (now - lastTick > 1000000000 / speed){
+                        lastTick = now;
+                        tick(gc);
+                    }
                 }
-                if (now - lastTick > 1000000000 / speed){
-                    lastTick = now;
-                }
-        }
+            }.start();
 
+        Scene scene = new Scene(root,with*cornerSize, height*cornerSize);
+
+        // control
+            scene.addEventFilter(KeyEvent.KEY_PRESSED,key ->{
+                if(key.getCode() == KeyCode.W){
+                    direction = Dir.up;
+                }
+                if(key.getCode() == KeyCode.A){
+                    direction = Dir.left;
+                }
+                if(key.getCode() == KeyCode.S){
+                    direction = Dir.down;
+                }
+                if(key.getCode() == KeyCode.D){
+                    direction = Dir.right;
+                }
+            });
+
+            // add start snake parts
+            snake.add(new Corner(with/2,height/2));
+            snake.add(new Corner(with/2,height/2));
+            snake.add(new Corner(with/2,height/2));
+
+            scene.getStylesheets().add(getClass().getResource("applications.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
      }
+
+     // tick
+
+    public static void tick(GraphicsContext gc){
+        if(gameOver){
+            gc.setFill(Color.RED);
+            gc.setFont(new Font("", 50));
+            gc.fillText("GAME OVER",100, 250);
+            return;
+        }
+        for(int i = snake.size() -1; i> 1; i--){
+            snake.get(i).x = snake.get(i-1).x;
+            snake.get(i).y = snake.get(i-1).y;
+        }
+        switch (direction)
+        {
+            case up:
+                snake.get(0).y--;
+                if(snake.get(0).y < 0){
+                    gameOver = true;
+                }
+                break;
+            case down:
+                snake.get(0).y++;
+                if(snake.get(0).y < height){
+                    gameOver = true;
+                }
+                break;
+            case left:
+                snake.get(0).x--;
+                if(snake.get(0).x < 0){
+                    gameOver = true;
+                }
+                break;
+            case right:
+                snake.get(0).x++;
+                if(snake.get(0).x < with){
+                    gameOver = true;
+                }
+                break;
+        }
+        // eat food
+
+        if(foodX == snake.get(0).x && foodY == snake.get(0).y){
+            snake.add(new Corner(-1,-1));
+            newFood();
+        }
+        // self destroy
+        for(int i = 1; i<snake.size();i++){
+            if(snake.get(0).x == snake.get(i).x && snake.get(0).y == snake.get(i).y){
+                gameOver = true;
+            }
+        }
+        // fill
+        // background
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0,0,with*cornerSize,height*cornerSize);
+
+        // score
+        gc.setFill(Color.WHITE);
+        gc.setFont(new Font("", 30));
+        gc.fillText("Score", +(speed-6), 10,30);
+
+    }
+
+    // random food color
+    Color cc = Color.WHITE;
+
+
+     //food
+    public static void newFood(){
+        start:while (true){
+            foodX = rand.nextInt(with);
+            foodY = rand.nextInt(height);
+            for(Corner c: snake){
+                if(c.x == foodX && c.y == foodY){
+                    continue start;
+                }
+            }
+            foodColor = rand.nextInt(5);
+            speed++;
+            break;
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
